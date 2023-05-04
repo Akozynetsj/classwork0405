a0e262e1ce570405693d6f7ef74a6ab919644d7f
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-04 19:09:34 +0300

    first example

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#
+#nкласи
+class Student:
+    print('hi')
+    height = 190
+    print(height)
+    def __init__(self, height = 150):
+        self.height = height
+oleg = Student()
+print(oleg.height)
